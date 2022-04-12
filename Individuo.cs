dc0b24979cfa19f25a5ac605ce592147990d07a1
ana004 <paulaana08002019@gmail.com>
2022-04-12 00:01:42 -0300

    commit inicial

--- Individuo.cs
+++ Individuo.cs
@@ -3,6 +3,8 @@ public class Individuo
     public string Nome { get; set; }
     public double Altura { get; set; }
     public double Peso { get; set; }
+    public double Massa { get; set; }
+    
     public Individuo(string Nome, double Altura, double Peso)
     {
         this.Nome = Nome;
@@ -12,23 +14,47 @@ public class Individuo
 
     public double Imc { get => Peso / (Altura*Altura); }
 
+    public string FaixaImc { 
+        get
+            {
+            if (Imc < 17)
+                return "Muito abaixo do peso";
+            else if (Imc >= 17 && Imc <= 18.49)
+                return "Abaixo do peso";
+            else if (Imc >= 18.50 && Imc <= 24.99)
+                return "Peso normal";
+            else if (Imc >= 25 && Imc <= 29.99)
+                return "Acima do peso";
+            else if (Imc >= 30 && Imc <= 34.99)
+                return "Obesidade I";
+            else if (Imc >= 35 && Imc <= 39.99)
+                return "Obesidade II (severa)";
 
-    /*
-    public double PesoGanhar
-    { 
-        get 
-        {
-           Massa = Math.Round(18.50*(Altura*Altura), 2);
-           PesoGanharPerder = Massa - Peso; 
-        }
+
+                return "Obesidade III (mórbida)";
+            } 
     }
 
-    public double PesoPerder
-    {
-        get 
-        {
-            Massa = Math.Round(24.99*(Altura*Altura), 2);
-            PesoGanharPerder = Peso - Massa;
-        }
-    }*/
+
+    public double MassaComecoFaixa { get => Massa = 18.50*Altura*Altura; }
+    
+    public double PesoGanhar { get => MassaComecoFaixa - Peso; }
+    
+    public double MassaFinalFaixa { get => Massa = 24.99*Altura*Altura; }
+    
+    public double PesoPerder { get => Peso - MassaFinalFaixa; }
+
+    public string PesoGanharPerder 
+    { 
+        get{
+                if (Imc < 18.50)
+                return "Massa necessária para o peso normal (IMC 18,50): " + MassaComecoFaixa + "\n" + "Massa necessária a ganhar para chegar ao peso normal (IMC 18,50): " + PesoGanhar;
+
+                if (Imc > 24.99) 
+                return "Massa necessária para o peso normal (IMC 24.99): " + MassaFinalFaixa + "\n" + "Massa necessária a perder para chegar ao peso normal (IMC 24.99): " + PesoPerder;
+
+                return " ";
+            } 
+    }
+   
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
-﻿Individuo individuo1 = new Individuo("Marco", 1.69, 60);
+﻿Individuo individuo1 = new Individuo("Marco", 1.69, 106);
 
 Console.WriteLine(individuo1.Nome);
 Console.WriteLine(individuo1.Altura);
 Console.WriteLine(individuo1.Peso);
 Console.WriteLine(individuo1.Imc);
+
+Console.WriteLine(individuo1.FaixaImc);
+Console.WriteLine(individuo1.PesoGanharPerder);
+
+Console.WriteLine("\n--------\n");
+
+Individuo individuo2 = new Individuo("Antonio", 1.87, 65);
+
+Console.WriteLine(individuo2.Nome);
+Console.WriteLine(individuo2.Altura);
+Console.WriteLine(individuo2.Peso);
+Console.WriteLine(individuo2.Imc);
+
+Console.WriteLine(individuo2.FaixaImc);
+Console.WriteLine(individuo2.PesoGanharPerder);
+
+
+
